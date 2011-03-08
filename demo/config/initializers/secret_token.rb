acb018500c3e7108f86ce682d22130cd299c5120
lakeBell <wildbolz.a@bluewin.ch>
2011-03-08 14:03:39 +0100

    first ruby application on the way

---
+++ demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '70beadd8f3397e554db44910ca296c39aa8be87d57d19d60fe9a3006a57d45c78e3ea7c19da0b2a6dced83f818d18b83d1610df2676c35d3168b2f039b6d4e1f'
